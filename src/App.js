f492bfcb9a899fdeef922257c45e4eb239af408e
NataliaGMbtm <natalia.gajda@gmail.com>
2023-08-01 18:56:28 +0200

    utworzenie templates + pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
 //5219cb6186dc8cdfa863dcdc035f17f8
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Movies from "./pages/Movies";
+import Serial from "./pages/Serial";
+import Person from "./pages/Person";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/serials" element={<Serial />} />
+          <Route path="/person" element={<Person />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Serials.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Serials = () => {
+  return <div>Serials</div>;
+};
+
+export default Serials;
