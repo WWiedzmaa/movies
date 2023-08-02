33bd8a2ee7788f7b0b531be06e18c9efe6a2f21e
NataliaGMbtm <natalia.gajda@gmail.com>
2023-08-02 18:27:07 +0200

    tworzymy hooka

---
+++ src/hooks/useMyHook.js
@@ -0,0 +1,36 @@
+//https://api.themoviedb.org/3/person/changes?api_key=5219cb6186dc8cdfa863dcdc035f17f8
+
+import { useEffect, useState } from "react";
+
+const url =
+  "https://api.themoviedb.org/3/person/changes?api_key=5219cb6186dc8cdfa863dcdc035f17f8";
+
+const useMyHook = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        setError("Nie udało się pobrać danych");
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError("Nie udało się pobrać danych");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error };
+};
+
+export default useMyHook;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Button } from "../components/Button";
+//import { Button } from "../components/Button";
+import { useMyHook } from "./../hooks/useMyHook";
 
 const HomePage = () => {
+  const [data, loading, error] = useMyHook(
+    "https://api.themoviedb.org/3/person/changes?api_key=5219cb6186dc8cdfa863dcdc035f17f8"
+  );
+  console.log(data);
   return <div></div>;
 };
 
