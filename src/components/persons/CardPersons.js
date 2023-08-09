4ac1715c680545a6e98b7756fc987553754bbc17
NataliaGMbtm <natalia.gajda@gmail.com>
2023-08-09 20:31:30 +0200

    dodanie styli do Person

--- src/components/persons/CardPersons.js
+++ src/components/persons/CardPersons.js
@@ -1,25 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import styles from "./CardPersons.module.css";
 
 const CardPersons = ({ person }) => {
+  const title = person.known_for.map((item) =>
+    item.title ? item.title : item.name
+  );
   return (
     <div>
       <Link
         to={`/person/${person.id + "-" + person.name.replaceAll(" ", "-")}`}
         state={{ obj: person }}
       >
-        <h3>{person.name}</h3>
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
-          alt="person_photo"
-        />
-        <p>
-          {person.known_for.map((item) =>
-            item.title ? item.title : item.name
-          )}
-          {/* {person.known_for[0].title},{person.known_for[1].title},
-        {person.known_for[2].title} */}
-        </p>
+        <Card className={styles.main} sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140, width: 200 }}
+            image={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              <span className={styles.name}>{person.name}</span>
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <span className={styles.text}>
+                {title[0]}, {title[1]}, {title[2]}
+              </span>
+            </Typography>
+          </CardContent>
+        </Card>
       </Link>
     </div>
   );
--- src/pages/Persons.js
+++ src/pages/Persons.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useMyHook from "./../hooks/useMyHook";
 import CardPersons from "../components/persons/CardPersons";
+import styles from "./Persons.module.css";
 
 const Person = () => {
   const { data, loading } = useMyHook(
@@ -8,7 +9,7 @@ const Person = () => {
   );
 
   return (
-    <div>
+    <div className={styles.main}>
       {!loading &&
         data.results.map((person) => (
           <CardPersons person={person} key={person.id} />
