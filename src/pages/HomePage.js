a3db5c19c0469bdf717f45dc838b5bed721b721c
NataliaGMbtm <natalia.gajda@gmail.com>
2023-08-09 19:52:11 +0200

    Poprawa widoczności listy wyszukiwania - hook

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,17 @@
 import React from "react";
 //import { Button } from "../components/Button";
-import useMyHook from './../hooks/useMyHook';
+import useMyHook from "./../hooks/useMyHook";
+import SampleComponent from "../hooks/useClenHook";
 
 const HomePage = () => {
-  const {data, loading, error} = useMyHook(
+  const { data, loading, error } = useMyHook(
     "https://api.themoviedb.org/3/person/changes"
   );
-  return <div></div>;
+  return (
+    <div>
+      <SampleComponent />
+    </div>
+  );
 };
 
 export default HomePage;
